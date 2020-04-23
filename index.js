018f59853be9fbaed8fcb7c433804f9574d61e2b
ShraddhaB <sbhingarde2017@gmail.com>
2020-04-23 10:16:07 +0530

    if statem,ent of the reducer fuction changed to switc statement

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-function createStore() {
+//Library
+function createStore(reducer) {
   // The store should have four parts
   // 1. The state
   // 2. Get the state.
@@ -12,14 +13,50 @@ function createStore() {
 
   const subscribe = (listener) => {
     listeners.push(listener);
-
     return () => {
       listeners = listeners.filter((l) => l !== listener);
     }
   }
 
-  return { getState, subscribe }
+  const dispatch = (action) => {
+    state = reducer(state, action);
+    listeners.forEach((listener) => listener())
+  }
+
+  return { getState, subscribe, dispatch }
 }
 
-const store = createStore();
+//App code
+function todos(state = [], action) {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return state.concat([action.todo]);
+
+    case 'REMOVE_TODO':
+      return state.filter((todo) => todo.id !== action.id)
+
+    case 'TOGGLE_TODO':
+      return state.map((todo) => todo.id !== action.id ? todo :
+        Object.assign({}, todo, { complete: !todo.complete }));
+
+    default:
+      state;
+  }
+}
+
+const store = createStore(todos);
+
+store.subscribe(() => {
+  console.log("the new state is ", store.getState());
+});
+
+store.dispatch({
+  type: 'ADD_TODO',
+  todo: {
+    id: 0,
+    name: "Learn React",
+    complete: false
+  }
+});
+
 
